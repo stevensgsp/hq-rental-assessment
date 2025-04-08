4ac21902b1bb8b9af41242f65b8f20702871e397
Steven Sucre <steven.g.s.p@gmail.com>
2025-04-08 15:12:17 -0400

    Installed spatie/laravel-query-builder. Updated ProductController to allow filters.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\Enums\FilterOperator;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function index(Request $request)
+    public function index()
     {
-        $products = Product::all(); // TODO: avoid lazy loading
+        $products = QueryBuilder::for(Product::class)
+            ->with(['category'])
+            ->allowedFilters([
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::exact('category', 'category.name'),
+                AllowedFilter::operator('price', FilterOperator::DYNAMIC)
+            ])
+            ->get();
 
         return ProductResource::collection($products);
     }
